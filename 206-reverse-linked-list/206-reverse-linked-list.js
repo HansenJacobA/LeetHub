2c97ad7b82ed1a7939cd85ec13c1fbf219395b1d
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-07-14 16:06:50 -0500

    Time: 103 ms (36.23%), Space: 44.6 MB (15.06%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -1,25 +1,3 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-
-//set the current to head
-//set the prev to null
-//set the next to null
-
-//while current is not null
-    //save next
-    //reverse
-    //advance prev and curr
-//return prev
-
 var reverseList = function(head) {
     let curr = head
     let prev = null
