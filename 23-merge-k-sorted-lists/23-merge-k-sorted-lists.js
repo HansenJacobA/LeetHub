d35f3e5f4af318d18deb07a0e2937725ff539d4a
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-01 00:36:39 -0500

    Time: 124 ms (81.46%), Space: 47.7 MB (51.29%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    const allVals = [];
+    for (let list of lists) {
+        while (list) {
+            allVals.push(list.val);
+            list = list.next;
+        }
+    }
+    allVals.sort((a, b) => a - b);
+    const head = new ListNode();
+    let runner = head;
+    for (const val of allVals) {
+        const node = new ListNode(val);
+        runner.next = node;
+        runner = runner.next;
+    }
+    return head.next;
+};
