bcbad6fb1bc4625ec971730c96fd1f4c5458cd3e
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-01 17:09:33 -0500

    Time: 98 ms (78.97%), Space: 45.8 MB (52.36%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const counts = {};
+    for (let i = 0; i < nums.length; i += 1) {
+        if (counts[nums[i]] === undefined) {
+            counts[nums[i]] = 1;
+            continue;
+        }
+        counts[nums[i]] += 1;
+    }
+    const inOrder = Object.entries(counts).sort((a, b) => a[1] - b[1]);
+    const topKdata = inOrder.slice(inOrder.length - k);
+    const topK = [];
+    for (let i = 0; i < topKdata.length; i += 1) {
+        topK.push(topKdata[i][0]);
+    }
+    
+    return topK;
+};
