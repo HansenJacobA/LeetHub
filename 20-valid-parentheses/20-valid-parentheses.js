5cca20b065a7768f29d8546a3aab968892336e29
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-28 13:56:12 -0500

    Time: 120 ms (19.49%), Space: 43.7 MB (13.57%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const key = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    const stack = [s[0]];
+    for (let i = 1; i < s.length; i += 1) {
+        if (key[s[i]] && stack.pop() !== key[s[i]]) return false;
+        if (!key[s[i]]) {
+            stack.push(s[i]);
+        }
+    }
+    return stack.length ? false : true;
+};
