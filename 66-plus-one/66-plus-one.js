96aaa743547b8b25ff4e4e032f5010e18f898f09
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-28 23:22:09 -0500

    Time: 113 ms (11.61%), Space: 41.9 MB (62.93%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -3,10 +3,5 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    const strNum = digits.join('');
-    const num = BigInt(strNum);
-    const newNum = num + BigInt(1);
-    const newNumStr = newNum.toString();
-    const newNumArr = newNumStr.split('');
-    return newNumArr;
+    return (BigInt(digits.join('')) + BigInt(1)).toString().split('');
 };
