7770d10c49479da73ab659593a37561edffbb11d
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-29 11:34:39 -0500

    Time: 104 ms (75.70%), Space: 46.8 MB (92.88%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.js
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.js
@@ -1,47 +1,24 @@
-const encode = strs => {
-    // const separator = "_";
-    // const result = strs.map(str => `${str.length}${separator}${str}`).join("");
-    // return result;
+/**
+ * Encodes a list of strings to a single string.
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var encode = function(strs) {
     return strs
 };
 
-const decode = s => {
+/**
+ * Decodes a single string to a list of strings.
+ *
+ * @param {string} s
+ * @return {string[]}
+ */
+var decode = function(s) {
     return s
-	// const separator = "_";
-	// const result = [];
-	// let i = 0;
-	// while (i < s.length) {
-	// let leftIdx = i;
-	// while (s[i] !== separator) {
-	// i++;
-	// }
-	// const len = Number(s.slice(leftIdx, i));
-	// const word = s.slice(i + 1, i + len + 1);
-	// result.push(word);
-	// i = i + len + 1; 
-	// }
-	// return result;
 };
 
-// const encode = strs => {
-//     const separator = "_";
-//     const result = strs.map(str => `${str.length}${separator}${str}`).join("");
-//     return result;
-// };
-
-// const decode = s => {
-// 	const separator = "_";
-//     const result = [];
-//     let i = 0;
-//     while (i < s.length) {
-//         let leftIdx = i;
-//         while (s[i] !== separator) {
-//             i++;
-//         }
-//         const len = Number(s.slice(leftIdx, i));
-//         const word = s.slice(i + 1, i + len + 1);
-//         result.push(word);
-//         i = i + len + 1; 
-//     }
-//     return result;
-// };
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
