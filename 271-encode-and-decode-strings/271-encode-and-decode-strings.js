10b124a4f54158457a1de02bce6f7363f46a75c0
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-29 11:25:32 -0500

    Time: 158 ms (18.99%), Space: 47.4 MB (65.36%) - LeetHub

---
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.js
@@ -0,0 +1,22 @@
+const encode = strs => {
+    const separator = "_";
+    const result = strs.map(str => `${str.length}${separator}${str}`).join("");
+    return result;
+};
+
+const decode = s => {
+	const separator = "_";
+    const result = [];
+    let i = 0;
+    while (i < s.length) {
+        let leftIdx = i;
+        while (s[i] !== separator) {
+            i++;
+        }
+        const len = Number(s.slice(leftIdx, i));
+        const word = s.slice(i + 1, i + len + 1);
+        result.push(word);
+        i = i + len + 1; 
+    }
+    return result;
+};
