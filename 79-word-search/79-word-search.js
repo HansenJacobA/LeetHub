2449c25a531e9d0ef091815f0129af8dbad1a002
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-29 00:20:55 -0500

    Time: 432 ms (91.25%), Space: 43.3 MB (60.74%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -9,33 +9,31 @@ var exist = function(board, word) {
         for (let j = 0; j < board[i].length; j += 1) {
             let wordFound = false;
             if (board[i][j] === word[0]) {
-                // ---------------------------------
                 
-function startLooking(wordIdx, row, col) {
-    const temp = board[row][col];
-    board[row][col] = '*';
-    if (wordIdx === lengthToMatch) {
-        wordFound = true;
-    }
-    const nextLetter = word[wordIdx];
-    wordIdx += 1;
-    if (validRowCol(row + 1, col, board) && board[row + 1][col]  === nextLetter && !wordFound) {
-        startLooking(wordIdx, row + 1, col);
-    }
-    if (validRowCol(row - 1, col, board) && board[row - 1][col]  === nextLetter && !wordFound) {
-        startLooking(wordIdx, row - 1, col);
-    }
-    if (validRowCol(row, col + 1, board) && board[row][col + 1]  === nextLetter && !wordFound) {
-        startLooking(wordIdx, row, col + 1);
-    }
-    if (validRowCol(row, col - 1, board) && board[row][col - 1]  === nextLetter && !wordFound) {
-        startLooking(wordIdx, row, col - 1);
-    }
-    board[row][col] = temp;
-    return;
-}
+            function startLooking(wordIdx, row, col) {
+                const temp = board[row][col];
+                board[row][col] = '*';
+                if (wordIdx === lengthToMatch) {
+                    wordFound = true;
+                }
+                const nextLetter = word[wordIdx];
+                wordIdx += 1;
+                if (validRowCol(row + 1, col, board) && board[row + 1][col]  === nextLetter && !wordFound) {
+                    startLooking(wordIdx, row + 1, col);
+                }
+                if (validRowCol(row - 1, col, board) && board[row - 1][col]  === nextLetter && !wordFound) {
+                    startLooking(wordIdx, row - 1, col);
+                }
+                if (validRowCol(row, col + 1, board) && board[row][col + 1]  === nextLetter && !wordFound) {
+                    startLooking(wordIdx, row, col + 1);
+                }
+                if (validRowCol(row, col - 1, board) && board[row][col - 1]  === nextLetter && !wordFound) {
+                    startLooking(wordIdx, row, col - 1);
+                }
+                board[row][col] = temp;
+                return;
+            }
                 
-                // ---------------------------------
                 startLooking(1, i, j);
             }
             if (wordFound) return true;
