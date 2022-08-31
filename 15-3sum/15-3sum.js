7d7aae0988b4f3520a4423b872688513318b4b1a
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-30 20:23:05 -0500

    Time: 132 ms (98.17%), Space: 53.3 MB (30.33%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,32 +3,26 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    // input - array nums
-    // output - array list of nums sum to 0
-    
-    // plan
-    // create cache obj of each num for constant time look up
-    // double for loop each num and look for the third num
-    // O(n) space and O(n^2) time
-    
-    const result = {};
-    const uniqueNums = nums.sort((a, b) => a - b);
-    for (let i = 0; i < uniqueNums.length - 2; i += 1) {
-        let left = i + 1;
-        let right = uniqueNums.length - 1;
-        while (left < right) {
-            const sum = uniqueNums[i] + uniqueNums[left] + uniqueNums[right];
-            if (sum > 0) {
-                right -= 1;
-            } else if (sum < 0) {
-                left += 1;
+    nums.sort((a, b) => a - b);
+    const result = [];
+    for (let i = 0; i < nums.length; i += 1) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        let l = i + 1;
+        let r = nums.length - 1;
+        while (l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
+            if (sum < 0) {
+                l += 1;
+            } else if (sum > 0) {
+                r -= 1;
             } else {
-                result[`${uniqueNums[i]}, ${uniqueNums[left]}, ${uniqueNums[right]}]`] = 
-                    [uniqueNums[i], uniqueNums[left], uniqueNums[right]];
-                left += 1;
-                right -= 1;
+                result.push([nums[i], nums[l], nums[r]]);
+                l += 1;
+                while (nums[l] == nums[l - 1]) {
+                       l += 1;
+                }
             }
-        } 
+        }
     }
-    return Object.values(result);
+    return result;
 };
