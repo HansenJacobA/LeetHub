139300e0e0d0b511e3d5c54128c486ea66791a35
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-08-30 21:40:02 -0500

    Time: 99 ms (71.62%), Space: 49.6 MB (58.98%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let maxVolume = 0;
+    let v = 0;
     let l = 0;
     let r = height.length - 1;
     while (l < r) {
         const w = r - l;
         const h = height[l] < height[r] ? height[l] : height[r];
-        maxVolume = w * h > maxVolume ? w * h : maxVolume;
+        v = w * h > v ? w * h : v;
         if (height[l] > height[r]) {
             r -= 1;
         } else {
             l += 1;
         }
     }
-    return maxVolume;
+    return v;
 };
