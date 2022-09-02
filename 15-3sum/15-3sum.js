7ee55036966fe81d42b1db785903f19f1426006a
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-02 18:50:53 -0500

    Time: 236 ms (44.81%), Space: 52.5 MB (66.76%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,23 +3,23 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+    const t = [];
     nums.sort((a, b) => a - b);
-    const res = [];
-    for (let i = 0; i < nums.length; i += 1) {
+    for (let i = 0; i < nums.length - 2; i += 1) {
         if (i > 0 && nums[i] == nums[i - 1]) continue;
         let l = i + 1;
         let r = nums.length - 1;
         while (l < r) {
-            if (nums[i] + nums[l] + nums[r] < 0) {
-                l += 1;
-            } else if (nums[i] + nums[l] + nums[r] > 0) {
+            if (nums[i] + nums[l] + nums[r] > 0) {
                 r -= 1;
+            } else  if (nums[i] + nums[l] + nums[r] < 0) {
+                l += 1;
             } else {
-                res.push([nums[i], nums[l], nums[r]]);
+                t.push([nums[i], nums[l], nums[r]]);
                 l += 1;
                 while (nums[l] == nums[l - 1]) l += 1;
             }
         }
     }
-    return res;
+    return t;
 };
