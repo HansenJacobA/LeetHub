d880119603eefbf29eb0f09c6781dec27c7c2aab
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-02 00:19:29 -0500

    Time: 76 ms (88.73%), Space: 45.2 MB (61.02%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -1,24 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 var maxDepth = function(root) {
-    let maxCount = 0;
-
-    const innerFunc = function(currentNode, currentCount) {
-
-      if(currentNode === null) return;
-
-        if(currentCount > maxCount) {
-          maxCount = currentCount;
-        }
-
-        if(currentNode?.left !== null) {
-          innerFunc(currentNode.left, currentCount+1);
-        }
-
-        if(currentNode?.right !== null) {
-          innerFunc(currentNode.right, currentCount+1);
-        }
-    }
-
-    innerFunc(root, 1);
-
-    return maxCount
+    if (root == null) return 0;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
