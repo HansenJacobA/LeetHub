9e9e5c87459c79422674e13452cc23f72762940b
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-02 15:58:55 -0500

    Time: 173 ms (13.93%), Space: 48.2 MB (21.43%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,18 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-    const letters = {};
-    for (let i = 0; i < s.length; i += 1) {
-        if (letters[s[i]] === undefined) {
-            letters[s[i]] = 1;
-            continue;
-        }
-        letters[s[i]] += 1;
-    }
-    for (let i = 0; i < t.length; i += 1) {
-        if (letters[t[i]] === undefined || letters[t[i]] === 0) return false;
-        letters[t[i]] -= 1;
-    }
-    return true;
+    return s.split('').sort().join('') == t.split('').sort().join('');
 };
