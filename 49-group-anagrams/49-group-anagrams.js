b4d67b0178686b0e9466131eade2a4c6d3542323
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-02 16:06:04 -0500

    Time: 122 ms (96.45%), Space: 52.9 MB (75.66%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,22 +3,14 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    // Input - array of strings
-    // Output - array of anagram groups
-    
-    // Create anagram collection obj
-    // Iterate through words
-    // Sort current word
-    // Add sorted word to collection as new array
-    // Or push into existing matching array
-    // Return collection as array
-    // O(nlogn) time and O(n) space
-    
-    const anagrams = {};
-    for (const word of strs) {
-        const sortedWord = word.split('').sort().join('');
-        anagrams[sortedWord] = anagrams[sortedWord] === undefined ?
-            [word] : [...anagrams[sortedWord], word];
+    const cache = {};
+    for (const str of strs) {
+        const sorted = str.split('').sort().join('');
+        if (cache[sorted] == undefined) {
+            cache[sorted] = [str];
+        } else {
+            cache[sorted].push(str);
+        }
     }
-    return Object.values(anagrams);
+    return Object.values(cache);
 };
