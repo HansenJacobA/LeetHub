3c1ef8cb743a812573b549c9535eef49d422f97a
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-02 23:41:54 -0500

    Time: 179 ms (6.76%), Space: 54.6 MB (5.40%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -7,10 +7,10 @@ var maxProfit = function(prices) {
     const left = new Array(prices.length);
     const right = new Array(prices.length);
     for (let i = 0; i < prices.length; i += 1) {
-        left[i] = left[i - 1] < prices[i] ? left[i - 1] : prices[i];
+        left[i] = Math.min(left[i - 1], prices[i]) || prices[i];
     }
     for (let i = prices.length - 1; i >= 0; i -= 1) {
-        right[i] = right[i + 1] > prices[i] ? right[i + 1] : prices[i];
+        right[i] = Math.max(right[i + 1], prices[i]) || prices[i];
     }
     for (let i = 0; i < prices.length; i += 1) {
         max = Math.max(max, right[i] - left[i]);
