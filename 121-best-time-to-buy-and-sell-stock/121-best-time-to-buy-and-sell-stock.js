5aae6473b746d72e6c2c2cc82acad792dba06fc7
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-02 23:53:14 -0500

    Time: 130 ms (46.06%), Space: 52 MB (16.11%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -2,4 +2,12 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(p) {let m = 0;let l = p[0];for(const n of p) {l = l < n ? l : n;m = m > n - l ? m : n - l;}return m;};
+var maxProfit = function(p) {
+    let m = 0;
+    let l = p[0];
+    for (const n of p) {
+        l = l < n ? l : n;
+        m = m > n - l ? m : n - l;
+    }
+    return m;
+};
