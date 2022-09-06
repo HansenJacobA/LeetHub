7ccc50e6b53909cb532422ccc7e5891978759392
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-05 19:16:01 -0500

    Time: 96 ms (60.70%), Space: 44.6 MB (17.07%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,41 +11,22 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if (!list1) return list2;
-    if (!list2) return list1;
-    
-    let p1 = list1;
-    let p2 = list2;
-    let prev = null;
-    
-    while (p1 && p2) {
-        if (p1.val <= p2.val) {
-            if (!prev) {
-                prev = p1;
-                p1 = p1.next;
-            } else {
-                prev.next = p1;
-                prev = prev.next;
-                p1 = p1.next;
-            }
-        } else {
-            if (!prev) {
-                prev = p2;
-                p2 = p2.next;
-            } else {
-                prev.next = p2;
-                prev = prev.next;
-                p2 = p2.next;
-            }
-        }
-        if (!p1) {
-            prev.next = p2;
-            break;
-        }
-        if (!p2) {
-            prev.next = p1;
-            break;
-        }
+    if (!list1 || !list2) return list1 ? list1 : list2;
+    const arr = [];
+    while (list1) {
+        arr.push(list1.val);
+        list1 = list1.next;
     }
-    return list1.val <= list2.val ? list1 : list2;
+    while (list2) {
+        arr.push(list2.val);
+        list2 = list2.next;
+    }
+    arr.sort((a, b) => a - b);
+    let newList = new ListNode(arr[0]);
+    let newHead = newList;
+    for (let i = 1; i < arr.length; i += 1) {
+        newList.next = new ListNode(arr[i]);
+        newList = newList.next;
+    }
+    return newHead;
 };
