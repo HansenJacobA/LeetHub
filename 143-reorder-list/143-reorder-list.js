ca94f3eb56883c7e3ccd80fb7f39589fd2ab549a
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-06 01:56:31 -0500

    Time: 126 ms (68.43%), Space: 53.4 MB (5.53%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    let left = head;
+    let right = head;
+    while (right) {
+        left = left.next;
+        right = right.next ? right.next.next : null;
+    }
+    let prev = null;
+    let curr = left;
+    let next = left ? left.next : null;
+    while (curr) {
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        next = curr ? curr.next : null;
+    }
+    let node = head;
+    console.log(prev)
+    console.log(node)
+    while (prev && node) {
+        let temp = node.next;
+        node.next = prev;
+        node = prev;
+        prev = prev.next;
+        if (!prev) {
+            node.next = temp;
+            node = temp;
+            node.next = null;
+            break;
+        }
+        node.next = temp;
+        node = temp;
+    }
+    return head;
+};
