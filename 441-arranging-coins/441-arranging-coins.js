46f7e51a8e286df8fac354344e7b820b30001915
Jacob Hansen <96079075+HansenJacobA@users.noreply.github.com>
2022-09-25 01:18:27 -0500

    Time: 117 ms (64.48%), Space: 44.5 MB (33.01%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.js
+++ 441-arranging-coins/441-arranging-coins.js
@@ -3,12 +3,5 @@
  * @return {number}
  */
 var arrangeCoins = function(n) {
-    let ans = 0;
-    let sum = 0;
-    for (let i = 0; i <= n; i += 1) {
-        sum += i;
-        if (sum > n) break;
-        ans = i;
-    }
-    return ans;
+    return Math.floor(Math.sqrt(2*n + .25) - .5);
 };
